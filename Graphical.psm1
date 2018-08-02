fb2e0aea5160fa4aa7376bd203d5c67b22e48021
Prateek Singh <prateeksingh1590@gmail.com>
2018-08-03 00:31:49 +0530

    Line chart included

--- Graphical.psm1
+++ Graphical.psm1
@@ -15,7 +15,7 @@ foreach ($item in $classList) {
     . "$BasePath\Source\classes\$item.ps1"
 }
 # Exporting the members and their aliases
-Export-ModuleMember *-* -Alias Graph
+Export-ModuleMember Show-Graph -Alias Graph
 
 #$Datapoints = (21..278|Get-Random -Count 50)
 #Show-Graph -Datapoints $Datapoints -GraphTitle "Avg. CPU utilization" -YAxisTitle "Percent" -Type Bar -YAxisStep 10 -XAxisStep 10
