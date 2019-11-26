bd36327262de2636ce2a09a80754f42934992ba7
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-26 10:28:16 -0800

    Add missing parameters on psm1

--- Graphical.psm1
+++ Graphical.psm1
@@ -1,6 +1,6 @@
-Get-ChildItem "$PSScriptRoot\src\" -Filter *.ps1 | ForEach-Object {
+Get-ChildItem -Path "$PSScriptRoot\src\" -Filter *.ps1 | ForEach-Object -Process {
     . $_.FullName
 }
 
 # Exporting the members and their aliases
-Export-ModuleMember Show-Graph -Alias Graph
+Export-ModuleMember -Function Show-Graph -Alias Graph
